dddabf4e4a5bd8c27180f059b297367e4ab99416
Shruti <sjawa006@wch136-24.cs.ucr.edu>
2021-04-10 21:19:18 -0700

    Completed part 2

--- sjawa006_lab2_part2.c
+++ sjawa006_lab2_part2.c
@@ -16,7 +16,6 @@ int main(void) {
     /* Insert DDR and PORT initializations */
     DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
     DDRC = 0xFF; PORTC = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
-    unsigned char tmpC = 0x00; // Temporary variable to hold the value of B
     unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
     unsigned char cntavail = 0x00;
     unsigned char i;
@@ -36,7 +35,7 @@ int main(void) {
 	}
 
 	// 3) Output   
-        PORTC = cntavail;    
+        PORTC = cntavail; //retains PORTC's bit 8
     }    
     return 0;
 }
